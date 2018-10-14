b181e20c9ca49f3e8e2948c38581bbc46bb58cdf
門平晴樹 <kadohiraharuki@wlan-napt-002.u-aizu.ac.jp>
2018-10-14 10:16:32 +0900

    取得したapiの情報を全て表示

--- app/src/main/java/com/example/kadohiraharuki/testapi/MainActivity.kt
+++ app/src/main/java/com/example/kadohiraharuki/testapi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kadohiraharuki.testapi
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,17 +11,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.squareup.moshi.Json
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import okio.BufferedSource
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,83 +45,8 @@ class MainActivity : AppCompatActivity() {
             Log.d("checkhit", HitAPITask().toString())
             //"+s.trim()+sort:followers")
 
-
         }
-
-
-        //試しリスト
-        val location = listOf(
-                "Hokkaido",
-                "Tokyo",
-                "Aizu",
-                "Kobe")
-        val repository = listOf(
-                "20",
-                "17",
-                "3",
-                "32"
-        )
-        val follow = listOf(
-                "32",
-                "42",
-                "14",
-                "15"
-        )
-        val follower = listOf(
-                "18",
-                "82",
-                "0",
-                "10"
-        )
-
-
-        data class UserData(val loc: String, val repository: String, val follow: String, val follower: String)
-
-        val users = List(location.size) { i -> UserData(location[i], repository[i], follow[i], follower[i]) }
-        //リストが上手くusersに格納されているかの確認
-        Log.d("data", users.toString())
-        data class ViewHolder(val locTextView: TextView, val repositoryTextView: TextView, val followTextView: TextView, val follwerTextView: TextView)
-
-        class UserListAdapter(context: Context, users: List<UserData>) : ArrayAdapter<UserData>(context, 0, users) {
-            private val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
-                var view = convertView
-                var holder: ViewHolder
-
-                if (view == null) {
-                    view = layoutInflater.inflate(R.layout.list_item, parent, false)
-                    holder = ViewHolder(
-                            view.findViewById(R.id.locTextView),
-                            view.findViewById(R.id.repositoryTextView),
-                            view.findViewById(R.id.followTextView),
-                            view.findViewById(R.id.followerTextView)
-                    )
-                    view.tag = holder
-                } else {
-                    holder = view.tag as ViewHolder
-                }
-
-                val user = getItem(position) as UserData
-                holder.locTextView.text = user.loc
-                holder.repositoryTextView.text = user.repository
-                holder.followTextView.text = user.follow
-                holder.follwerTextView.text = user.follower
-
-
-                return view
-            }
-        }
-
-
-        //listViewにUserListAdapterをセット
-        val arrayAdapter = UserListAdapter(this, users)
-        val listView: ListView = findViewById(R.id.listView)
-        listView.adapter = arrayAdapter
-
-
     }
-
 
     inner class HitAPITask : AsyncTask<String, String, Array<Array<String?>>?>() {
 
@@ -156,48 +87,44 @@ class MainActivity : AppCompatActivity() {
                 Log.d("CHECK", buffer.toString())
 
 
-                val jsonText = buffer.toString()
+               val jsonText = buffer.toString()
 
 
                 val parentJsonObj = JSONObject(jsonText)
                 Log.d("CHECK2", parentJsonObj.toString())
-
                 val parentJSONOArray = parentJsonObj.getJSONArray("items")
 
-                var arrayd = Array(3, { arrayOfNulls<String>(4) })
+                val namearr = arrayListOf<String>()
+                val avatararr = arrayListOf<String>()
+                val repoarr = arrayListOf<String>()
+                for(i in 0..(parentJSONOArray.length() - 1)){
 
-                val firstJsonObj = parentJSONOArray.getJSONObject(0)
-                val loginname: ArrayList<String> = arrayListOf(firstJsonObj.getString("login"))
-                val avatar: ArrayList<String> = arrayListOf(firstJsonObj.getString("avatar_url"))
-                val rep: ArrayList<String> = arrayListOf(firstJsonObj.getString("repos_url"))
-                Log.d("CHECKlo1", loginname.toString())
-                Log.d("CHECKav1", avatar.toString())
-                Log.d("CHECKrep1", rep.toString())
-
-                val secondJsonObj = parentJSONOArray.getJSONObject(1)
-                loginname.add(secondJsonObj.getString("login"))
-                avatar.add(secondJsonObj.getString("avatar_url"))
-                rep.add(secondJsonObj.getString("repos_url"))
-
-                val thirdJsonObj = parentJSONOArray.getJSONObject(2)
-                loginname.add(thirdJsonObj.getString("login"))
-                avatar.add(thirdJsonObj.getString("avatar_url"))
-                rep.add(thirdJsonObj.getString("repos_url"))
-
-                val fourthJsonObj = parentJSONOArray.getJSONObject(3)
-                loginname.add(fourthJsonObj.getString("login"))
-                avatar.add(fourthJsonObj.getString("avatar_url"))
-                rep.add(fourthJsonObj.getString("repos_url"))
-
-                arrayd[0] = Array(loginname.size) { i -> loginname[i] }
-                arrayd[1] = Array(avatar.size) { i -> avatar[i] }
-                arrayd[2] = Array(rep.size) { i -> rep[i] }
-
-                Log.d("Checkarr", arrayd[2][3])
+                    val useritems = parentJSONOArray.getJSONObject(i)
+                    val name = useritems.getString("login")
+                    val avatar = useritems.getString("avatar_url")
+                    val repo= useritems.getString("repos_url")
+                    namearr.add(name)
+                    avatararr.add(avatar)
+                    repoarr.add(repo)
 
 
-                //Stringでreturnしてあげましょう。
+                }
+
+                Log.d("nameconf", namearr[15])
+                Log.d("avatarconf", avatararr[13])
+                Log.d("repoconf", repoarr[11])
+
+                var arrayd = Array(3, { arrayOfNulls<String>(namearr.size) })
+
+                arrayd[0] = Array(namearr.size) { i -> namearr[i] }
+                arrayd[1] = Array(avatararr.size) { i -> avatararr[i] }
+                arrayd[2] = Array(repoarr.size) { i -> repoarr[i] }
+
+                Log.d("sizzz" , namearr.size.toString())
+                Log.d("arrayd", arrayd[2][1].toString())
+
                 return arrayd
+
 
                 //ここから下は、接続エラーとかJSONのエラーとかで失敗した時にエラーを処理する為のものです。
             } catch (e: MalformedURLException) {
@@ -231,13 +158,27 @@ class MainActivity : AppCompatActivity() {
             }
             Log.d("CHECKresu", result[2][1])
 
+            val location = listOf(
+                    "Hokkaido"
+            )
+            val repository = listOf(
+                    "20"
+            )
+            val follow = listOf(
+                    "32"
+            )
+            val follower = listOf(
+                    "18"
+            )
 
-            data class UsersData(val name: String?, val userImgView: String?)
 
-            val users = List(result[0].size) { i -> UsersData(result[0][i], result[1][i]) }
+
+            data class UsersData(val name: String?, val userImgView: String?, val loc: String, val repository: String, val follow: String, val follower: String)
+
+            val users = List(result[0].size) { i -> UsersData(result[0][i], result[1][i], location[0], repository[0], follow[0], follower[0]) }
             Log.d("CHECKuser", users.toString())
 
-            data class ViewHolder(val nameTextView: TextView, val UserImgView: ImageView)
+            data class ViewHolder(val nameTextView: TextView, val UserImgView: ImageView, val locTextView: TextView, val repositoryTextView: TextView, val followTextView: TextView, val follwerTextView: TextView)
 
             class UserAdapter(context: Context, users: List<UsersData>) : ArrayAdapter<UsersData>(context, 0, users) {
                 private val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -250,7 +191,11 @@ class MainActivity : AppCompatActivity() {
                         view = layoutInflater.inflate(R.layout.list_item, parent, false)
                         holder = ViewHolder(
                                 view.findViewById(R.id.nameTextView),
-                                view.findViewById(R.id.userImgView)
+                                view.findViewById(R.id.userImgView),
+                                view.findViewById(R.id.locTextView),
+                                view.findViewById(R.id.repositoryTextView),
+                                view.findViewById(R.id.followTextView),
+                                view.findViewById(R.id.followerTextView)
                         )
                         view.tag = holder
                     } else {
@@ -260,6 +205,10 @@ class MainActivity : AppCompatActivity() {
                     val usersp = getItem(position) as UsersData
                     holder.nameTextView.text = usersp.name
                     Picasso.with(context).load(usersp.userImgView).into(holder.UserImgView)
+                    holder.locTextView.text = usersp.loc
+                    holder.repositoryTextView.text = usersp.repository
+                    holder.followTextView.text = usersp.follow
+                    holder.follwerTextView.text = usersp.follower
 
 
                     return view
@@ -268,17 +217,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             val array2Adapter = UserAdapter(applicationContext, users)
-            val listView2: ListView = findViewById(R.id.listView)
-            listView2.adapter = array2Adapter
+            val listView: ListView = findViewById(R.id.listView)
+            listView.adapter = array2Adapter
 
 
             listView.setOnItemClickListener { parent, view, position, id ->
                 val rep = result[2][position]
                 Log.d("reprlt", rep)
                 HitRep().execute(rep)
-
-                //val intent = Intent(applicationContext, webview::class.java)
-                //startActivity(intent)
 
             }
 
@@ -318,27 +264,20 @@ class MainActivity : AppCompatActivity() {
                 Log.d("CHECKREPO", buffer.toString())
 
                 var arrayrepo = Array(2, { arrayOfNulls<String>(4) })
-                val RepoJsonobj = JSONArray(buffer.toString())
-                val firstRepo = RepoJsonobj.getJSONObject(0)
-                val Reponame: ArrayList<String> = arrayListOf(firstRepo.getString("name"))
-                val giturl: ArrayList<String> = arrayListOf(firstRepo.getString("svn_url"))
-                Log.d("Checkfirstrepo", Reponame.toString())
 
-                val SecondRepo = RepoJsonobj.getJSONObject(1)
-                Reponame.add(SecondRepo.getString("name"))
-                giturl.add(SecondRepo.getString("svn_url"))
-                Log.d("reponame2", Reponame[1])
+                val RepoJsonarr = JSONArray(buffer.toString())
+                val reponamearr = arrayListOf<String>()
+                val repourlarr = arrayListOf<String>()
+                for(i in 0..(RepoJsonarr.length() - 1)){
 
-                val ThirdRepo = RepoJsonobj.getJSONObject(2)
-                Reponame.add(ThirdRepo.getString("name"))
-                giturl.add(ThirdRepo.getString("svn_url"))
-
-                val ForthRepo = RepoJsonobj.getJSONObject(3)
-                Reponame.add(ForthRepo.getString("name"))
-                giturl.add(ForthRepo.getString("svn_url"))
-
-                arrayrepo[0] = Array(Reponame.size) { i -> Reponame[i] }
-                arrayrepo[1] = Array(giturl.size) { i -> giturl[i] }
+                    val useritems = RepoJsonarr.getJSONObject(i)
+                    val reponame = useritems.getString("name")
+                    val repourl= useritems.getString("svn_url")
+                    reponamearr.add(reponame)
+                    repourlarr.add(repourl)
+                }
+                arrayrepo[0] = Array(reponamearr.size) { i -> reponamearr[i] }
+                arrayrepo[1] = Array(repourlarr.size) { i -> repourlarr[i] }
 
                 return arrayrepo
 
@@ -358,8 +297,6 @@ class MainActivity : AppCompatActivity() {
                     e.printStackTrace()
                 }
             }
-            //textView.text =("失敗")
-            //失敗した時はnullやエラーコードなどを返しましょう。
             return null
 
         }
